df9230dc610935b79cdc83cd0b0b49ec39766c2c
ushiboy <ushiboy.dev@gmail.com>
2021-11-27 18:03:27 +0900

    add app run test

--- tests/_helper.py
+++ tests/_helper.py
@@ -8,6 +8,7 @@ class MockIO(IOInterface):
     def __init__(self):
         self.setup_out_channels: List[int] = []
         self.output_channel_histories: List[Tuple[int, int]] = []
+        self.finalized = False
 
     def initialize(self) -> None:
         raise NotImplementedError
@@ -22,4 +23,4 @@ class MockIO(IOInterface):
         self.output_channel_histories.append((channel, 0))
 
     def finalize(self) -> None:
-        raise NotImplementedError
+        self.finalized = True
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,34 @@
-from fpl._app import app_process
+import time
+from threading import Thread
+
+from fpl._app import Application, app_process
+from fpl._exception import TerminatedException
 from fpl._led import LED
 
 from ._helper import MockIO
+
+
+def test_app_run():
+    channel = 17
+    io = MockIO()
+    state = {"count": 0}
+
+    def wrap(led: LED, led_status: bool) -> bool:
+        state["count"] += 1
+        if state["count"] > 3:
+            raise TerminatedException
+        return app_process(led, led_status)
+
+    def t():
+        Application(io, channel, wrap, 0.1).run()
+
+    th = Thread(target=t, daemon=True)
+    th.start()
+    time.sleep(0.5)
+    th.join(1)
+    assert io.output_channel_histories == [
+        (channel, 1), (channel, 0), (channel, 1)]
+    assert io.finalized
 
 
 def test_app_process():
